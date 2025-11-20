442e9997e4bba5b9d153e83ba7449431cba8c93a
Danilo Jr Arizala <dailydev@192.168.1.2>
2025-11-20 21:36:06 +0800

    Configure/added coupon model

---
+++ lib/database/models/coupon.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+import { unique } from "next/dist/build/utils";
+
+const couponSchema = new mongoose.Schema(
+    {
+        coupon: {
+            type: String,
+            trim: true,
+            unique: true,
+            uppercase: true,
+            required: true,
+            minLength: 4,
+            maxLength: 10,
+        },
+
+        vendor: {
+            type: Object,
+        },
+
+        startDate: {
+            type: String,
+            required: true,
+        },
+
+        endDate: {
+            type: String,
+            required: true,
+        },
+
+        discount: {
+            type: Number,
+            required: true,
+        },
+    },
+
+    {
+        timestamps: true,
+    }
+);
+
+const Coupon = mongoose.models.Coupon || mongoose.model("Coupon", couponSchema);
+
+export default Coupon;
